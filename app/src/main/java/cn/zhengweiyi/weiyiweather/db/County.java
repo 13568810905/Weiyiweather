1c170e9b19bd98c8510fb97561a2b6046f1d42e0
13568810905 <vipzhengweiyi@vip.qq.com>
2017-12-06 09:07:17 +0800

    上课编辑之后，更新代码，第二阶段未完成

---
+++ app/src/main/java/cn/zhengweiyi/weiyiweather/db/County.java
@@ -0,0 +1,45 @@
+package cn.zhengweiyi.weiyiweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+
+    private String countyName;      //县名
+
+    private String weatherId;       //天气ID
+
+    private int cityId;             //当前所属城市id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId(int cityId) {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
